d13369e2e4ef6aaa8feb6227774da4c2ae13ac27
Anne <anna301488@gmail.com>
2025-05-22 09:59:40 +0300

    !

--- front.js
+++ front.js
@@ -1,9 +1,30 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const toggleLinks = document.querySelectorAll('.expandable-link');
+//   toggleLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//       const contentId = link.getAttribute('data-toggle');
+//       const content = document.getElementById('expandableContent' + contentId.slice(-1));
+      
+//       if (!content) return; // Проверка на наличие элемента
+//       // Проверяем, есть ли класс 'show'
+//       const isVisible = content.classList.contains('show');
+//       if (isVisible) {
+//         content.classList.remove('show');
+//         link.textContent = 'Развернуть';
+//       } else {
+//         content.classList.add('show');
+//         link.textContent = 'Свернуть';
+//       }
+//     });
+//   });
+// });
+
 document.addEventListener('DOMContentLoaded', () => {
   const toggleLinks = document.querySelectorAll('.expandable-link');
   toggleLinks.forEach(link => {
     link.addEventListener('click', () => {
-      const contentId = link.getAttribute('data-toggle');
-      const content = document.getElementById('expandableContent' + contentId.slice(-1));
+      const contentId = link.getAttribute('data-toggle'); // Получаем значение data-toggle
+      const content = document.getElementById(contentId); // Используем его напрямую для поиска элемента
       
       if (!content) return; // Проверка на наличие элемента
       // Проверяем, есть ли класс 'show'
