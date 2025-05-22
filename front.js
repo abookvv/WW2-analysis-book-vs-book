b18cd93456c53c83be3fbf81d65c367744b7bcc0
Anne <anna301488@gmail.com>
2025-05-22 11:21:38 +0300

    роботают все блокиЭ

--- front.js
+++ front.js
@@ -1,41 +1,12 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const toggleLinks = document.querySelectorAll('.expandable-link');
-//   toggleLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//       const contentId = link.getAttribute('data-toggle');
-//       const content = document.getElementById('expandableContent' + contentId.slice(-1));
-      
-//       if (!content) return; // Проверка на наличие элемента
-//       // Проверяем, есть ли класс 'show'
-//       const isVisible = content.classList.contains('show');
-//       if (isVisible) {
-//         content.classList.remove('show');
-//         link.textContent = 'Развернуть';
-//       } else {
-//         content.classList.add('show');
-//         link.textContent = 'Свернуть';
-//       }
-//     });
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
   const toggleLinks = document.querySelectorAll('.expandable-link');
   toggleLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      const contentId = link.getAttribute('data-toggle'); // Получаем значение data-toggle
-      const content = document.getElementById(contentId); // Используем его напрямую для поиска элемента
-      
-      if (!content) return; // Проверка на наличие элемента
-      // Проверяем, есть ли класс 'show'
-      const isVisible = content.classList.contains('show');
-      if (isVisible) {
-        content.classList.remove('show');
-        link.textContent = 'Развернуть';
-      } else {
-        content.classList.add('show');
-        link.textContent = 'Свернуть';
-      }
-    });
+      link.addEventListener('click', () => {
+          const contentId = link.getAttribute('data-toggle');
+          const content = document.getElementById(contentId);
+          if (!content) return;
+          const isVisible = content.classList.toggle('show');
+          link.textContent = isVisible ? 'Свернуть' : 'Развернуть';
+      });
   });
 });
